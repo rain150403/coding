a509ea766edea10b2f32b2a64a44bb1be27ac549
RL_learning <2890035926@QQ.com>
2017-10-23 10:56:55 +0800

    Update 1023_1num_appear_once.cpp

--- CLASSIC/1023_1num_appear_once.cpp
+++ CLASSIC/1023_1num_appear_once.cpp
@@ -37,3 +37,39 @@ public:
     }
      
 };
+
+
+/* 数组中唯一只出现一次的数字 
+题目：在一个数组中除了一个数字只出现一次之外，其他数字都出现了三次。请找出那个吃出现一次的数字。*/
+
+#include <cstdio>
+#include <exception>
+
+int FindNumberAppearingOnce(int numbers[], int length)
+{
+    if(numbers == nullptr || length <= 0)
+        throw new std::exception("Invalid input.");
+
+    int bitSum[32] = {0};
+    for(int i = 0; i < length; ++i)
+    {
+        int bitMask = 1;
+        for(int j = 31; j >= 0; --j)
+        {
+            int bit = numbers[i] & bitMask;
+            if(bit != 0)
+                bitSum[j] += 1;
+
+            bitMask = bitMask << 1;
+        }
+    }
+
+    int result = 0;
+    for(int i = 0; i < 32; ++i)
+    {
+        result = result << 1;
+        result += bitSum[i] % 3;
+    }
+
+    return result;
+}
