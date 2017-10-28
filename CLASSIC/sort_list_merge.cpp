a71d359b49a3d3e104b51a417db6808835bc2e86
RL_learning <2890035926@QQ.com>
2017-10-28 20:43:38 +0800

    Update sort_list_merge.cpp

--- CLASSIC/sort_list_merge.cpp
+++ CLASSIC/sort_list_merge.cpp
@@ -20,6 +20,20 @@ Sort a linked list in O(n log n) time using constant space complexity.
 3）写出mergesort函数，实现上述步骤。
 */
 
+/*
+  考点：
+  1. 快慢指针；2. 归并排序。
+  此题经典，需要消化吸收。
+  复杂度分析:
+             T(n)            拆分 n/2, 归并 n/2 ，一共是n/2 + n/2 = n
+            /    \           以下依此类推：
+          T(n/2) T(n/2)      一共是 n/2*2 = n
+         /    \  /     \
+        T(n/4) ...........   一共是 n/4*4 = n
+ 
+       一共有logn层，故复杂度是 O(nlogn)
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
