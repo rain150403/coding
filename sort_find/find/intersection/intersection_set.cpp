83c0a92a2180f7d8982b17c9bafcb66ea73d5d7f
RL_learning <2890035926@QQ.com>
2017-11-27 11:23:28 +0800

    Update intersection_set.cpp

--- sort_find/find/intersection/intersection_set.cpp
+++ sort_find/find/intersection/intersection_set.cpp
@@ -1,1 +1,46 @@
-ad
+/*
+将数组一存放到set里，扫描数组二，看有没有元素在set里面，如果有，那就把这个共同的元素存放在set2里面，最后再把set2里面的数存放在一个vector里面。输出即可。
+*/
+
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+/// 349. Intersection of Two Arrays
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        set<int> record;
+        for( int i = 0 ; i < nums1.size() ; i ++ )
+            record.insert(nums1[i]);
+
+        set<int> resultSet;
+        for( int i = 0 ; i < nums2.size() ; i ++ )
+            if( record.find( nums2[i] ) != record.end() )
+                resultSet.insert( nums2[i] );
+
+        vector<int> resultVector;
+        for(set<int>::iterator iter = resultSet.begin() ; iter != resultSet.end() ; iter ++ )
+            resultVector.push_back( *iter );
+
+        return resultVector;
+    }
+};
+
+int main() {
+
+    int nums1[] = {1, 2, 2, 1};
+    vector<int> vec1(nums1, nums1 + sizeof(nums1)/sizeof(int));
+
+    int nums2[] = {2, 2};
+    vector<int> vec2(nums2, nums2 + sizeof(nums2)/sizeof(int));
+
+    vector<int> res = Solution().intersection(vec1, vec2);
+    for(int i = 0 ; i < res.size() ; i ++ )
+        cout<<res[i]<<" ";
+    cout<<endl;
+
+    return 0;
+}
