41977650624e7854548f377b48cc809a0778b91a
RL_learning <2890035926@QQ.com>
2017-11-28 15:44:48 +0800

    Update binary_search.cpp

--- sort_find/find/binary_search.cpp
+++ sort_find/find/binary_search.cpp
@@ -35,3 +35,83 @@ int binary_search( int* a, int len, int goal ){
 
 	return -1;
 }
+
+//多看几遍，就记住了
+#include <iostream>
+#include <cmath>
+#include <cassert>
+#include <ctime>
+#include "util.h"
+
+using namespace std;
+
+template<typename T>
+int binarySearch( T arr[], int n, T target ){
+
+    int l = 0, r = n; // target在[l...r)的范围里
+    while( l < r ){    // 当 l == r时,区间[l...r)是一个无小区间
+        int mid = l + (r-l)/2;
+        if( arr[mid] == target ) return mid;
+        if( target > arr[mid] )
+            l = mid + 1;    // target在[mid+1...r)中; [l...mid]一定没有target
+        else// target < arr[mid]
+            r = mid;        // target在[l...mid)中; [mid...r)一定没有target
+    }
+
+    return -1;
+}
+
+int main() {
+
+    int n = pow(10,7);
+    int* data = MyUtil::generateOrderedArray(n);
+
+    clock_t startTime = clock();
+    for( int i = 0 ; i < n ; i ++ )
+        assert( i == binarySearch(data, n, i) );
+    clock_t endTime = clock();
+
+    cout<<"binarySearch2 test complete."<<endl;
+    cout<<"Time cost: "<<double(endTime - startTime)/CLOCKS_PER_SEC<<" s"<<endl;
+    return 0;
+}
+
+//要想把二分搜索写完全正确，也要用心
+#include <iostream>
+#include <cmath>
+#include <cassert>
+#include <ctime>
+#include "util.h"
+
+using namespace std;
+
+template<typename T>
+int binarySearch( T arr[], int n, T target ){
+
+    int l = 0, r = n-1; // 在[l...r]的范围里寻找target
+    while( l <= r ){    // 当 l == r时,区间[l...r]依然是有效的
+        int mid = l + (r-l)/2;
+        if( arr[mid] == target ) return mid;
+        if( target > arr[mid] )
+            l = mid + 1;  // target在[mid+1...r]中; [l...mid]一定没有target
+        else    // target < arr[mid]
+            r = mid - 1;  // target在[l...mid-1]中; [mid...r]一定没有target
+    }
+
+    return -1;
+}
+
+int main() {
+
+    int n = pow(10,7);
+    int* data = MyUtil::generateOrderedArray(n);
+
+    clock_t startTime = clock();
+    for( int i = 0 ; i < n ; i ++ )
+        assert( i == binarySearch(data, n, i) );
+    clock_t endTime = clock();
+
+    cout<<"binarySearch test complete."<<endl;
+    cout<<"Time cost: "<<double(endTime - startTime)/CLOCKS_PER_SEC<<" s"<<endl;
+    return 0;
+}
